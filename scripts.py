61c00ba1d862e200df6a27e3f4ed47ffb58c93d3
Nicolas Flamme <nclsf87@gmail.com>
2022-07-07 17:43:56 +0300

    Улучшил обработку неверных данных

--- scripts.py
+++ scripts.py
@@ -2,54 +2,62 @@ import random
 
 from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 
-from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation
+from datacenter.models import (Schoolkid, Mark, Chastisement, Lesson,
+                               Commendation)
 
 
-def get_kid_by_name(kid_name: str) -> Schoolkid:
-    if not kid_name:
-        print('Вы забыли ввести имя ученика.')
-        exit(1)
-    try:
-        kid = Schoolkid.objects.get(full_name__contains=kid_name)
-    except MultipleObjectsReturned:
-        print('Существует несколько учеников с таким именем.')
-        print('Пожалуйста, введите более точное имя.')
-        exit(1)
-    except ObjectDoesNotExist:
-        print('Ученика с таким именем не существует.')
-        exit(1)
-    else:
-        print(f'Ученик по имени "{kid.full_name}" найден.')
-        return kid
+def get_kid_by_name() -> Schoolkid:
+    while True:
+        kid_name = input('Введите имя ученика: ')
+        try:
+            kid = Schoolkid.objects.get(full_name__contains=kid_name)
+        except MultipleObjectsReturned:
+            print('Существует несколько учеников с таким именем.')
+            print('Пожалуйста, введите более точное имя.')
+            continue
+        except ObjectDoesNotExist:
+            print('Ученика с таким именем не существует.')
+            continue
+        else:
+            print(f'Ученик по имени "{kid.full_name}" найден.')
+            return kid
 
 
-def fix_marks(kid_name: str):
-    schoolkid = get_kid_by_name(kid_name)
-    target_kid_bad_marks = Mark.objects.filter(schoolkid=schoolkid,
-                                               points__in=[2, 3])
-    for bad_mark in target_kid_bad_marks:
-        bad_mark.points = 5
-        bad_mark.save()
+def get_subject_last_lesson() -> Lesson:
+    while True:
+        subject_name = input('Введите название предмета: ').capitalize()
+        class_number = int(input('Введите номер класса: '))
+        class_letter = input('Введите букву класса: ').upper()
+        target_class_subj_lesson = Lesson.objects.filter(
+            year_of_study=class_number,
+            group_letter=class_letter,
+            subject__title=subject_name
+        ).order_by('-date').first()
+        if not target_class_subj_lesson:
+            print('Урок не был найден. Введите верные данные.')
+            continue
+        else:
+            return target_class_subj_lesson
+
+
+def fix_marks():
+    schoolkid = get_kid_by_name()
+    Mark.objects.filter(
+        schoolkid=schoolkid, points__in=[2, 3]
+    ).update(points=5)
     print('Оценки исправлены.')
 
 
-def remove_chastisements(kid_name: str):
-    schoolkid = get_kid_by_name(kid_name)
+def remove_chastisements():
+    schoolkid = get_kid_by_name()
     target_kid_chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
     target_kid_chastisements.delete()
     print('Замечания удалены.')
 
 
-def create_commendation(kid_name: str, subject_name: str):
-    if not subject_name:
-        print('Вы забыли ввести название предмета.')
-        exit(1)
-    schoolkid = get_kid_by_name(kid_name)
-    target_class_subj_lesson = Lesson.objects \
-        .filter(year_of_study=6,
-                group_letter='А',
-                subject__title=subject_name) \
-        .order_by('-date').first()
+def create_commendation():
+    schoolkid = get_kid_by_name()
+    target_class_subj_lesson = get_subject_last_lesson()
     praise_lines = [
         'Молодец!',
         'Отлично!',
@@ -82,16 +90,14 @@ def create_commendation(kid_name: str, subject_name: str):
         'Ты многое сделал, я это вижу!',
         'Теперь у тебя точно все получится!',
     ]
-    try:
-        date = target_class_subj_lesson.date
-    except AttributeError:
-        print('Вы ошиблись в названии предмета.')
-        exit(1)
-    else:
-        subject = target_class_subj_lesson.subject
-        teacher = target_class_subj_lesson.teacher
-        Commendation.objects.create(text=random.choice(praise_lines),
-                                    created=date, schoolkid=schoolkid,
-                                    subject=subject,
-                                    teacher=teacher)
-        print(f'Похвала к последнему уроку по предмету "{subject_name}" создана.')
+    date = target_class_subj_lesson.date
+    subject = target_class_subj_lesson.subject
+    teacher = target_class_subj_lesson.teacher
+    Commendation.objects.create(
+        text=random.choice(praise_lines),
+        created=date,
+        schoolkid=schoolkid,
+        subject=subject,
+        teacher=teacher
+    )
+    print(f'Похвала к последнему уроку по предмету "{subject.title}" создана.')
